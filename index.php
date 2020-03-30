a990f6588d0793aa056005684c945e0d1b8353f5
hana-ji <55686380+hana-ji@users.noreply.github.com>
2020-03-30 14:42:08 +0900

    데이터베이스를 php로 제어 (보안은 나중에 배움)

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <!doctype html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>WEB</title>
-    </head>
-    <body>
-        <h1>WEB</h1>
-    <ol>
-        <li>HTML</li>
-    </ol>
-    <h2>Welcome</h2>
-    Lorem ikljfaaadsada apple asdasdasd dasdasdasdasdasdwad
-    </body>
+<head>
+    <meta charset="utf-8">
+    <title>WEB</title>
+</head>
+<body>
+<h1>WEB</h1>
+<ol>
+    <li>HTML</li>
+</ol>
+<a href="create.php">create</a>
+<h2>Welcome</h2>
+Lorem ikljfaaadsada apple asdasdasd dasdasdasdasdasdwad
+</body>
 </html>
---
+++ process_create.php
@@ -0,0 +1,25 @@
+<?php
+// 데이터베이스 접속
+$conn = mysqli_connect(
+    'localhost',
+    'root',
+    '123456',
+    'opentutorials');
+$sql = "
+    INSERT INTO topic
+    (title, description, created)
+    VALUES(
+        '{$_POST['title']}',
+        '{$_POST["description"]}',
+        NOW()
+    )
+";
+//접속 된 데이터베이스 서버로 sql문 전송
+//첫 번째 자리엔 데이터베이스 접속 정보를 담고있는 변수를 적어야함
+$result = mysqli_query($conn, $sql);
+if($result === false){
+    echo '저장하는 과정에서 문제가 생겼습니다.';
+} else {
+    echo  '저장에 성공했습니다. <a href="index.php">돌아가기</a>';
+}
+?>
