80f00d11fb108e2188a14996991198096d861022
hana-ji <55686380+hana-ji@users.noreply.github.com>
2020-04-01 11:54:13 +0900

    글목록

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
+<?php
+$conn = mysqli_connect(
+    'localhost',
+    'root',
+    '123456',
+    'opentutorials');
 
+// 글 목록은 모든 항목 가져오기
+$sql = "SELECT * FROM topic";
+//데이타베이스 서버에 전송하는 api (접속식별자, sql)를 result에 담음
+$result = mysqli_query($conn, $sql);
+$list = '';
+while($row = mysqli_fetch_array($result)) {
+    // <li><a href="index.php?id=15">MySQL</a></li> 이런 코드를 프로그래밍 적으로 생성
+    $list = $list . "<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+}
+?>
 <!doctype html>
 <html>
 <head>
@@ -8,11 +24,8 @@
 <body>
 <h1>WEB</h1>
 <ol>
-    <li>HTML</li>
-    <li>CSS</li>
-    <li>JavaScript</li>
-    <li>PHP</li>
-    <li>MySQL</li>
+<!--   이렇게 해도 <?php echo $list; ?> 랑 똑같은 표현임   -->
+    <?=$list?>
 </ol>
 <a href="create.php">create</a>
 <h2>Welcome</h2>
