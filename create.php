f34f68151661d011cce29ba99b3285114d62dd5c
hana-ji <55686380+hana-ji@users.noreply.github.com>
2020-04-01 14:00:35 +0900

    보안 mysqli_real_escape_string

--- create.php
+++ create.php
@@ -18,16 +18,13 @@ $article = array(
     'description'=>'Hello, web'
 );
 if(isset($_GET['id'])){
-    // 아이디 값만 따오기
-    $sql = "SELECT * FROM topic WHERE id={$_GET['id']}";
+    $filtered_id = mysqli_real_escape_string($conn, $_GET['id']);
+    $sql = "SELECT * FROM topic WHERE id={$filtered_id}";
     $result = mysqli_query($conn, $sql);
-    // 서버에 전달한 값을 배열화 근데 반복문 안쓸거임
     $row = mysqli_fetch_array($result);
-    // id값이 존재하면 덮어씌움
     $article['title'] = $row['title'];
     $article['description'] = $row['description'];
 }
-//print_r($article); 잘나오는지 확인
 ?>
 <!doctype html>
 <html>
@@ -38,15 +35,12 @@ if(isset($_GET['id'])){
 <body>
 <h1><a href="index.php">WEB</a></h1>
 <ol>
-    <!--   이렇게 해도 <?php echo $list; ?> 랑 똑같은 표현임   -->
     <?=$list?>
 </ol>
-<!-- action: 사용자가 submit버튼을 눌렀을 때 이동하는 곳 -->
     <form action="process_create.php" method="post">
         <p><input type="text" name="title" placeholder="title"></p>
         <p><textarea name="description" placeholder="description"></textarea></p>
         <p><input type="submit"></p>
     </form>
-
 </body>
 </html>
--- index.php
+++ index.php
@@ -18,16 +18,13 @@ $article = array(
     'description'=>'Hello, web'
 );
 if(isset($_GET['id'])){
-    // 아이디 값만 따오기
-    $sql = "SELECT * FROM topic WHERE id={$_GET['id']}";
+    $filtered_id = mysqli_real_escape_string($conn, $_GET['id']);
+    $sql = "SELECT * FROM topic WHERE id={$filtered_id}";
     $result = mysqli_query($conn, $sql);
-    // 서버에 전달한 값을 배열화 근데 반복문 안쓸거임
     $row = mysqli_fetch_array($result);
-    // id값이 존재하면 덮어씌움
     $article['title'] = $row['title'];
     $article['description'] = $row['description'];
 }
-//print_r($article); 잘나오는지 확인
 ?>
 <!doctype html>
 <html>
@@ -38,7 +35,6 @@ if(isset($_GET['id'])){
 <body>
 <h1><a href="index.php">WEB</a></h1>
 <ol>
-<!--   이렇게 해도 <?php echo $list; ?> 랑 똑같은 표현임   -->
     <?=$list?>
 </ol>
 <a href="create.php">create</a>
