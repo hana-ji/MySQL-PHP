9971c111c2530dc89584feca5bca4f16f528214c
hana-ji <55686380+hana-ji@users.noreply.github.com>
2020-04-01 12:17:36 +0900

    글 목록 누르면 내용바뀌기

--- create.php
+++ create.php
@@ -1,3 +1,34 @@
+<?php
+$conn = mysqli_connect(
+    'localhost',
+    'root',
+    '123456',
+    'opentutorials');
+
+$sql = "SELECT * FROM topic";
+$result = mysqli_query($conn, $sql);
+$list = '';
+while($row = mysqli_fetch_array($result)) {
+    $list = $list . "<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+}
+
+//기본 값
+$article = array(
+    'title'=>'Welcome',
+    'description'=>'Hello, web'
+);
+if(isset($_GET['id'])){
+    // 아이디 값만 따오기
+    $sql = "SELECT * FROM topic WHERE id={$_GET['id']}";
+    $result = mysqli_query($conn, $sql);
+    // 서버에 전달한 값을 배열화 근데 반복문 안쓸거임
+    $row = mysqli_fetch_array($result);
+    // id값이 존재하면 덮어씌움
+    $article['title'] = $row['title'];
+    $article['description'] = $row['description'];
+}
+//print_r($article); 잘나오는지 확인
+?>
 <!doctype html>
 <html>
 <head>
@@ -5,10 +36,11 @@
     <title>WEB</title>
 </head>
 <body>
-    <h1>WEB</h1>
-    <ol>
-        <li>HTML</li>
-    </ol>
+<h1><a href="index.php">WEB</a></h1>
+<ol>
+    <!--   이렇게 해도 <?php echo $list; ?> 랑 똑같은 표현임   -->
+    <?=$list?>
+</ol>
 <!-- action: 사용자가 submit버튼을 눌렀을 때 이동하는 곳 -->
     <form action="process_create.php" method="post">
         <p><input type="text" name="title" placeholder="title"></p>
--- index.php
+++ index.php
@@ -5,15 +5,29 @@ $conn = mysqli_connect(
     '123456',
     'opentutorials');
 
-// 글 목록은 모든 항목 가져오기
 $sql = "SELECT * FROM topic";
-//데이타베이스 서버에 전송하는 api (접속식별자, sql)를 result에 담음
 $result = mysqli_query($conn, $sql);
 $list = '';
 while($row = mysqli_fetch_array($result)) {
-    // <li><a href="index.php?id=15">MySQL</a></li> 이런 코드를 프로그래밍 적으로 생성
     $list = $list . "<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
 }
+
+//기본 값
+$article = array(
+    'title'=>'Welcome',
+    'description'=>'Hello, web'
+);
+if(isset($_GET['id'])){
+    // 아이디 값만 따오기
+    $sql = "SELECT * FROM topic WHERE id={$_GET['id']}";
+    $result = mysqli_query($conn, $sql);
+    // 서버에 전달한 값을 배열화 근데 반복문 안쓸거임
+    $row = mysqli_fetch_array($result);
+    // id값이 존재하면 덮어씌움
+    $article['title'] = $row['title'];
+    $article['description'] = $row['description'];
+}
+//print_r($article); 잘나오는지 확인
 ?>
 <!doctype html>
 <html>
@@ -22,14 +36,13 @@ while($row = mysqli_fetch_array($result)) {
     <title>WEB</title>
 </head>
 <body>
-<h1>WEB</h1>
+<h1><a href="index.php">WEB</a></h1>
 <ol>
 <!--   이렇게 해도 <?php echo $list; ?> 랑 똑같은 표현임   -->
     <?=$list?>
 </ol>
 <a href="create.php">create</a>
-<h2>Welcome</h2>
-Lorem issum dolor sit amet, consectetur
-adipisicing elit
+<h2><?=$article['title']?></h2>
+<?=$article['description']?>
 </body>
 </html>
